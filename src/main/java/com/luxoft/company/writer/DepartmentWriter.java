2ea51005d064e630073029f72ddfdce18e678369
Alexander Yarkin <alexander.yarkin33@gmail.com>
2021-12-24 11:16:46 +0200

    Split print method to get string with all departments and get string with one department

--- src/main/java/com/luxoft/company/writer/DepartmentWriter.java
+++ src/main/java/com/luxoft/company/writer/DepartmentWriter.java
@@ -13,14 +13,32 @@ public class DepartmentWriter {
         this.outputStream = outputStream;
     }
 
-    public void write(List<Department> departments) throws IOException {
-        StringBuilder stringBuilder = new StringBuilder("Колличество работников в каждом департаменте:\n");
+    public void writeByEmployeesCount(List<Department> departments) throws IOException {
+        String departmentsWithEmployeeCount = getDepartmentsByEmployeeCount(departments);
+        outputStream.write("Колличество работников в каждом департаменте:\n".getBytes());
+        outputStream.write(departmentsWithEmployeeCount.getBytes());
+        outputStream.flush();
+    }
 
+    // package-private for testing
+    String getDepartmentsByEmployeeCount(List<Department> departments) {
+        StringBuilder stringBuilder = new StringBuilder();
         for (Department department : departments) {
-            String line = String.format("%20s -> %d\n", department.getTitle(), department.getEmployeesCount());
+            String line = getDepartmentByEmployeeCount(department);
             stringBuilder.append(line);
         }
+        return stringBuilder.toString();
+    }
 
-        outputStream.write(stringBuilder.toString().getBytes());
+    String getDepartmentByEmployeeCount(Department department) {
+        if(department == null) {
+            throw new IllegalStateException("Department is null. Department must be initialized before printing");
+        }
+
+        if(department.getTitle() == null) {
+            throw new IllegalStateException("Department title must be initialized");
+        }
+
+        return String.format("%20s -> %d\n", department.getTitle(), department.getEmployeesCount());
     }
 }
